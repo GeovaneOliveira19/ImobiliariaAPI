a6d6d85afdab88239a7014235dfca0a1633a0b3d
GeovaneOliveira19 <geovanesilva.mail@gmail.com>
2022-12-04 12:32:12 -0300

    Commit inicial

---
+++ Imovel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImobiliariaAPI
+{
+    public class Imovel
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string Comentarios { get; set; } = string.Empty;
+
+        public int Tipo { get; set; }
+
+        public TipoImovel? TipoImovel { get; set; }
+    }
+}
---
+++ TipoImovel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImobiliariaAPI
+{
+    public class TipoImovel
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Tipo { get; set; } = string.Empty;
+    }
+}
